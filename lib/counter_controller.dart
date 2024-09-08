de66987ad4f0f93a473bccbc51206545bf916ad1
Iamsunil06 <sunilyadav67804@gmail.com>
2024-09-08 17:31:01 +0530

    counterapp

---
+++ lib/counter_controller.dart
@@ -0,0 +1,20 @@
+// ignore: depend_on_referenced_packages
+import 'package:get/get.dart';
+
+class CounterController extends GetxController {
+  int count = 0;
+  inc() {
+    count++;
+    update();
+  }
+
+  dec() {
+    --count;
+    update();
+  }
+
+  reset() {
+    count = 0;
+    update();
+  }
+}
